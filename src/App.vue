2c28b6c49b03144b22a7216bbfbfbb1d8827944a
k <kunkunzzzz@outlook.com>
2020-12-01 17:25:04 +0800

    封装请求模块

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,20 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import request from '@/utils/request'
+
+request({
+  method: 'GET',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(res => {
+  console.log(res)
+})
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
 <style lang="scss" scoped></style>

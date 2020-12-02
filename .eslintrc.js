6b2eb5b86a5dc04ee741af5268193d03e75c65d2
k <kunkunzzzz@outlook.com>
2020-12-02 11:34:52 +0800

    不校验函数后面的空格

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   // 自定义编码校验规则
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0
   }
 }

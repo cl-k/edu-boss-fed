8324d26bdeb7a238dad00b4e57738ffa02fb83b4
k <kunkunzzzz@outlook.com>
2020-12-02 18:42:59 +0800

    使用请求拦截器统一设置 Token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,24 @@
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   // 配置选项 baseURL, timeout...
 })
 
 // 请求拦截器
+request.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  // 在这里通过改写 config 配置信息来实现业务功能的统一处理
+  const { user } = store.state
+  if (user && user.access_token) {
+    config.headers.Authorization = user.access_token
+  }
+  // 注意：这里一定要返回 config,否则请求就发不出去
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 

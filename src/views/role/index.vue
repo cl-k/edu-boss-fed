211dd5b16a3d5c9c89e440937ac223b77ba8bbbf
k <kunkunzzzz@outlook.com>
2020-12-07 16:28:50 +0800

    添加角色

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>RoleIndex</div>
+  <div class="role">
+    <role-list />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import RoleList from './components/List.vue'
 
 export default Vue.extend({
-  name: 'RoleIndex'
+  name: 'RoleIndex',
+  components: {
+    RoleList
+  }
 })
 </script>
 

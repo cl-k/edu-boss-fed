dbeba8300cd4c13bbf70078ec2b7046eeb1d3b1a
k <kunkunzzzz@outlook.com>
2020-12-08 14:23:18 +0800

    课程管理列表及处理课程上下架

---
+++ src/services/course.ts
@@ -0,0 +1,23 @@
+/*
+ * @Description: 课程相关请求模块
+ * @Author: k
+ */
+import request from '@/utils/request'
+
+// 分页查询课程信息
+export const getQueryCourses = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/course/getQueryCourses',
+    data
+  })
+}
+
+// 课程上下架
+export const changeState = (params: any) => {
+  return request({
+    method: 'GET',
+    url: '/boss/course/changeState',
+    params
+  })
+}
--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>CourseIndex</div>
+  <div class="course">
+    <course-list />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import CourseList from './components/CourseList.vue'
 
 export default Vue.extend({
-  name: 'CourseIndex'
+  name: 'CourseIndex',
+  components: {
+    CourseList
+  }
 })
 </script>
 
